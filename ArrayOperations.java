b1c317de5b247c724e905ea8ab19b122af51909f
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2024-05-25 16:10:40 +0530

    Add files via upload

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,87 +1,85 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;;
 
-public class ArrayOperations {
-    public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,2,12,22};
-        ArrayOperations a = new ArrayOperations();
-        a.findElement(2,arr);
-        a.findAllOccurence(2,arr);
-        int[] reversed_Array = a.reverse(arr);
-        a.sort(arr);  //in-place using bubble sort
-        int[] sorted_array = a.sortResult(arr);
-        a.binarySearch(3,sorted_array);  
-        a.swap(2,3,arr);
-        a.searchInRange(2,7,3,arr);
-        a.searchAllInRange(1,19,2,arr);
-        //a.rotateClockwise(2,arr);
-        // a.rotateAnticlockwise(2,arr);
-        int[] sub_arr = {6,7,18,9};
-        a.subArray(3,9,arr);
-        a.containsSubArray(arr,sub_arr);       
-             
-        
-        // map() (will explain what is this)
-        // maxWindow (will explain what is this)
+// map() (will explain what is this)
+// maxWindow (will explain what is this)
+
+class ArrayOperations{
+    public static void main(String [] args){
+        int[] arr = {1,2,3,4,9,8,7,5,29,16,12,23,25,19,9,99,100};
+        method_implementation mi = new method_implementation();
+
+        int target = 9;
+        int find = mi.find(target,arr);
+        System.out.println("Element "+ target+" found at index : "+find);
+
+        List<Integer> found = mi.findAll(target,arr);
+        System.out.println("Element "+target +" found at indices : "+found);
+
+        // mi.sortInPlace(arr);
+        // System.out.println("Array sorted in-place : "+Arrays.toString(arr));
+
+        int[] sorted_array = mi.sort(arr);
+        System.out.println("Array before sorting : "+Arrays.toString(arr));
+        System.out.println("array after sorting : " + Arrays.toString(sorted_array));
+
+        int target_index = mi.binarySearch(target,arr);
+        System.out.println("Element  found at index : "+target_index);
+
+        mi.reverse(arr);
+        System.out.println("Reversed array : " + Arrays.toString(arr));
+
+        int[] sub_arr = mi.subArray(8,4,arr);
+        System.out.println(" Formed subArray : "+Arrays.toString(sub_arr));
+
+        int pos1 = 3, pos2 = 9;
+        int[] swapped_Array = mi.swap(pos1,pos2,arr);
+        System.out.println("Array after swapping positions  "+pos1+" and "+pos2 + " : "+Arrays.toString(swapped_Array));
+
+        int start = 2 , end = 14;
+        int index_of_target = mi.searchInRange(start, end, target,arr);
+        System.out.println("Index of "+target+" between the range  "+start+ " and "+end+ " is : "+index_of_target);
+
+        List<Integer>  indices_of_target = mi.searchAllInRange(start,end,target,arr);
+        System.out.println("Element  "+target+ " present in indices  : "+indices_of_target);
+
+        int [] mini_arr = { 12,16,20,19};
+        boolean isContains = mi.containsSubArray(mini_arr,arr);     
+        System.out.println("Contains subarray : "+isContains); 
+
+        int rotate = 2;
+        mi.rotate_anticlockwise(arr,rotate);
+        System.out.println("After rotating anticlockwise "+ rotate+ " times : "+Arrays.toString(arr));
+
+        mi.rotate_clockwise(arr,rotate);
+        System.out.println("After rotating clockwise "+ rotate+ " times : "+Arrays.toString(arr));
+    
+    }
+}
+
+class method_implementation{
+
+    int find(int target,int[] a){
+        for(int i = 0; i < a.length;i++){
+            if(a[i] == target){
+                return i;
+            }
+        }
+        return -1;
     }
 
-    void findElement(int target, int[] arr){
-        int index = -1;
-        for(int i = 0; i < arr.length; i++){
+    List<Integer> findAll(int target, int[] arr){
+        List<Integer> indices = new ArrayList<Integer>();
+        for(int i = 0; i < arr.length;i++){
             if(arr[i] == target){
-                index = i;
-                break;
+                indices.add(i);
             }
         }
-        if(index == -1){
-            System.out.println("\nElement not found ");
-        }
-        else{
-            System.out.println("\n Element  found at index : "+ index);
-        }
-
+        return indices;
     }
 
-    void findAllOccurence(int target, int[] arr){
-        List<Integer> index = new ArrayList<>();
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == target){
-                index.add(i);
-            }
-        }
-        if(index.size() == 0){
-            System.out.println("Element not found ");
-        }
-        else{
-            System.out.println("Element  found at indices : ");
-            for(int i : index){
-                System.out.print(i + " ");
-            }
-        }
-    }
-
-    int[] reverse(int[] arr){
-        int [] reversed_array = new int[arr.length];
-        for(int i = 0; i < reversed_array.length; i++){
-            reversed_array[i] = arr[i]; 
-        }
-        for(int i = 0 ; i < reversed_array.length/2; i++){
-            int temp = reversed_array[i];
-            reversed_array[i] = reversed_array[reversed_array.length - i -1];
-            reversed_array[reversed_array.length -i -1] = temp;
-        }
-        System.out.println("\nreversed array : ");
-        for(int i = 0 ; i < reversed_array.length; i++){
-            System.out.print(reversed_array[i]+ " ");
-        }
-        return reversed_array;
-    }
-
-    void sort(int[] arr){
-        int [] original = new int[arr.length];
-        for(int i = 0; i < original.length; i++){
-            original[i] = arr[i]; 
-        }
+    void sortInPlace(int[] original){
         for(int i = 0; i < original.length-1; i++){
             boolean flag = true;
             for(int j = 0; j < original.length - i -1; j++){
@@ -96,25 +94,21 @@ public class ArrayOperations {
                 break;
             }
         }
-        System.out.println("\nSorted array : ");
-        for(int i : original){
-            System.out.print(i+ " ");
-        }
-
     }
 
-    int[] sortResult(int[] arr){
-        int [] result = new int[arr.length];
-        for(int i = 0; i < result.length; i++){
-            result[i] = arr[i]; 
+    int[] sort(int[] arr){
+        int[] original = new int[arr.length];
+        for(int i = 0; i < arr.length;i++){
+            original[i] = arr[i];
         }
-        for(int i = 0; i < result.length-1; i++){
+
+        for(int i = 0; i < original.length-1; i++){
             boolean flag = true;
-            for(int j = 0; j < result.length - i -1; j++){
-                if(result[j] > result[j+1]){
-                    int temp = result[j];
-                    result[j] = result[j+1];
-                    result[j+1] = temp;
+            for(int j = 0; j < original.length - i -1; j++){
+                if(original[j] > original[j+1]){
+                    int temp = original[j];
+                    original[j] = original[j+1];
+                    original[j+1] = temp;
                     flag = false;
                 }
             }
@@ -122,125 +116,142 @@ public class ArrayOperations {
                 break;
             }
         }
-        return result;
-
+        return original;
     }
 
-    void binarySearch(int target, int[] arr){
-        int left = 0 , right = arr.length-1;
-        int index = -1;
+    void reverse(int[] arr){
+        int left = 0, right = arr.length-1;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left ++;
+            right --;
+        }
+    }
+
+    int binarySearch(int target , int[] a){
+        int[] arr = sort(a);
+        int left = 0, right = arr.length -1;
         while(left <= right){
-            int mid = left + ((right - left)/2);
+            int mid = left + ((right-left)/2);
             if(arr[mid] == target){
-                index = mid;
-                break;
+                return mid;
             }
-            else if(target > arr[mid]){
-                left = mid+1;
+            else if(arr[mid]>target){
+                right = mid;
             }
             else{
-                right = mid -1;
+                left = mid+1;
             }
         }
-        if(index == -1){
-            System.out.println("Element not found .");
-        }
-        else{
-            System.out.println("\nElement found at index : "+index);
-        }
-
+        return -1;
     }
 
-    void swap(int posX, int posY, int[] a){
-        if(posX < 0 || posX >= a.length || posY < 0 || posY >=a.length){
-            System.out.println("Invalid posX / posY value " );
-            return;
+    int[] subArray(int start, int end, int[] arr){
+        
+        if(start == 0 && end == arr.length){
+            return arr;
         }
-        int temp = a[posX];
-        a[posX] = a[posY];
-        a[posY] = temp;
-        System.out.println("Array after swap : ");
-        for(int i : a){
-            System.out.print(i + " ");
+        if(start < 0 || start >= arr.length || end >= arr.length || end < 0 || start > end){
+            return null;
         }
+        int[] sub = new int[end-start];
+        for(int i = start, j = 0 ; j < sub.length; j++,i++){
+            sub[j] = arr[i]; 
+        }
+        return sub;
     }
 
-    void searchInRange(int start, int end, int target,int[] arr){
-        if(start < 0 || start >= arr.length || end < 0 || end >=arr.length || start > end){
-            System.out.println("Invalid start / end value " );
-            return;
+    int[] swap(int pos1, int pos2,int[]arr){
+        if(pos1 < 0 || pos1 >= arr.length || pos2 < 0 || pos2 >= arr.length){
+            return null;
         }
-        int index = -1;
-        for(int i = start; i <= end; i++){
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+        return arr;
+    }
+
+    int searchInRange(int start, int end,int target, int[] arr){
+        if(start < 0 || start >= arr.length || end < 0 || end >= arr.length || start > end){
+            return -1;
+        }
+        for(int i = start; i < end; i++){
             if(arr[i] == target){
-                index = i;
-                break;
+                return i;
             }
         }
-        if( index == -1){
-            System.out.println("Element not found . ");
-        }
-        else{
-            System.out.println("Element found at index : "+ index);
-        }
-
+        return -1;
     }
 
-    void searchAllInRange(int start, int end, int target, int[] arr){
-        if(start < 0 || start >= arr.length || end < 0 || end >=arr.length || start > end){
-            System.out.println("Invalid start / end value " );
-            return;
+    List<Integer> searchAllInRange(int start, int end, int target, int[] arr){
+        List<Integer> indices = new ArrayList<Integer>();
+        if(start < 0 || start >= arr.length || end >=arr.length || end < 0 || start > end){
+            return null;
         }
-        List<Integer> result = new ArrayList<>();
-        for(int i = start; i <= end; i++){
+        for(int i = start ; i < end ; i++){
             if(arr[i] == target){
-                result.add(i);
-            }
+                indices.add(i);
+            } 
         }
-        if( result.size() == 0){
-            System.out.println("Element not found . ");
-        }
-        else{
-            System.out.println("Element found at indices: ");
-            for(int i : result){
-                System.out.print(i + " ");
-            }
-        }
+        return indices;
     }
 
-    void subArray(int start, int end, int[] arr){
-        if(start < 0 || start >= arr.length || end < 0 || end > arr.length ){
-            System.out.println("Invalid start / end values ");
-            return;
+    boolean containsSubArray(int[] sub, int[] arr){
+        if(sub.length > arr.length){
+            return false;
         }
-        int[] sub = new int[end - start];
-        for( int i = start, j = 0; j < sub.length; i++,j++){
-            sub[j] = arr[i];
-        }
-        System.out.println("Sub array formed in range "+start + " , "+end+ "  is :");
-        for(int i : sub){
-            System.out.print(i + " ");
-        }
-     }
-
-     void containsSubArray(int[] arr, int[] sub_arr){
-        if(sub_arr.length > arr.length){
-            System.out.println("does not contain sub array . ");
-            return;
-        }
-        for(int i = 0 ; i < arr.length;i++){
-            if(arr[i]==sub_arr[0]){
-                for(int j = 0;j < sub_arr.length;j++,i++){
-                    if(arr[i] != sub_arr[j]){
-                        System.out.println("\ndoes not contain sub array . ");
-                        return;
-                    }
+        int i = 0, j = 0;
+        while(i < arr.length && j < sub.length){
+            if(arr[i] == sub[j]){
+                i++;
+                j++;
+                if(j == sub.length){
+                    return true;
                 }
             }
+            else{
+                i++;
+                j = 0;
+            }
         }
-        System.out.println("\nSub array is present.");
-     }
+        return false;
+    }
 
+    void reverse(int[] arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
-    
+    void rotate_anticlockwise(int[] arr, int k){
+        
+        if(k > arr.length){
+            k = k % arr.length;
+        }
+        if(k == 0 || k == arr.length){
+            return;
+        }
+        reverse(arr, 0, k-1);
+        reverse(arr, k , arr.length-1 );
+        reverse(arr, 0 , arr.length-1);
+    }
+
+    void rotate_clockwise(int [] arr, int k){
+        if(k > arr.length){
+            k = k % arr.length;
+        }
+        if(k == 0 || k == arr.length){
+            return;
+        }
+        reverse(arr, 0 , arr.length -1);
+        reverse(arr, 0, k-1);
+        reverse(arr,k , arr.length -1);     
+    }
+
 }
